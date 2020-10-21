bfa20228c09f9a10b1ec576d27d1e391e56c82e8
ponmalar6596 <ponmalar6596@gmail.com>
2020-10-21 08:53:29 -0700

    indexof

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var a1=[1,5,6,7,9];
+var a=[1,5,6,7,9];
 /*var x;
 x=a.reverse(); //reverse
 console.log(a);*/
@@ -8,8 +8,9 @@ console.log(x);*/
 /*var x;
 x=a.join("*"); //join
 console.log(x);*/
-var a2=[1,2,3,4,5];
-var x=a1.concat(a2);
-console.log(x);
+/*var a2=[1,2,3,4,5];
+var x=a1.concat(a2); //concat
+console.log(x); */
+console.log(a.indexOf(5)); //indexof
 
 
