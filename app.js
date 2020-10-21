5574367e2df631fe972bc622d547842d9de79141
ponmalar6596 <ponmalar6596@gmail.com>
2020-10-21 08:47:53 -0700

    concat

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-var a=[1,5,6,7,9];
+var a1=[1,5,6,7,9];
 /*var x;
-x=a.reverse(); //slice
+x=a.reverse(); //reverse
 console.log(a);*/
 /*var x=a
-x.splice(2,0,8,10);
+x.splice(2,0,8,10); //splice
 console.log(x);*/
-var x;
-x=a.join("*");
+/*var x;
+x=a.join("*"); //join
+console.log(x);*/
+var a2=[1,2,3,4,5];
+var x=a1.concat(a2);
 console.log(x);
+
 
