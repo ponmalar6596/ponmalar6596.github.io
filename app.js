cb5cb11d0d7715835c873a37d9a32a8cfd22e870
ponmalar6596 <ponmalar6596@gmail.com>
2020-10-21 08:37:52 -0700

    join

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var a=[1,5,6,7,9];
-var x;
+/*var x;
 x=a.reverse(); //slice
-console.log(a);
+console.log(a);*/
+/*var x=a
+x.splice(2,0,8,10);
+console.log(x);*/
+var x;
+x=a.join("*");
+console.log(x);
 
