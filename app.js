3475a796d30c98079af098844203b515378eb016
ponmalar6596 <ponmalar6596@gmail.com>
2020-10-21 08:58:05 -0700

    string

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var a=[1,5,6,7,9];
 /*var x;
-x=a.reverse(); //reverse
+x=a.reverse();                    //reverse
 console.log(a);*/
 /*var x=a
-x.splice(2,0,8,10); //splice
+x.splice(2,0,8,10);                //splice
 console.log(x);*/
 /*var x;
-x=a.join("*"); //join
+x=a.join("*");                      //join
 console.log(x);*/
 /*var a2=[1,2,3,4,5];
-var x=a1.concat(a2); //concat
+var x=a1.concat(a2);                //concat
 console.log(x); */
-console.log(a.indexOf(5)); //indexof
+/*console.log(a.indexOf(5)); */     //indexof
+var x;
+x=a.toString();                      //string
+console.log(x);
 
 
